d145e8c757ddde0b94ea3e5ccebf71ec5a49ee26
Nikola <nikola@datainsights.ae>
2018-08-24 16:51:21 +0400

    android issue fixed

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobPackage.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobPackage.java
@@ -13,19 +13,19 @@ import java.util.List;
 
 public class RNFetchBlobPackage implements ReactPackage {
 
-    @Override
+    // @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new RNFetchBlob(reactContext));
         return modules;
     }
 
-    @Override
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
+    // @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
